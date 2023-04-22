89d39da31063a6ac0de9b7ea2f2853714a32bdbb
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-04-22 11:02:23 +0200

    fix: cookie config

--- src/consts/cookie.ts
+++ src/consts/cookie.ts
@@ -6,6 +6,6 @@ import { envVariables } from '../utils/envVariables';
 export const cookie: CookieOptions = {
   httpOnly: true,
   maxAge: ms('7 days'),
-  sameSite: 'lax',
+  sameSite: envVariables.PRODUCTION === 'false' ? 'lax' : 'none',
   secure: envVariables.PRODUCTION === 'false' ? false : true,
 };
